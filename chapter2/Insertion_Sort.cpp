9eaa8fd27190adb47e877f2a0563b0aa2e21c6d5
Michael Liu <chyliu14@lzu.edu.cn>
2016-07-13 15:35:05 +0800

    Update Insertion_Sort.cpp

--- chapter2/Insertion_Sort.cpp
+++ chapter2/Insertion_Sort.cpp
@@ -4,18 +4,9 @@
 
 using namespace std;
 
-void Insertion_Sort() {
-	static default_random_engine        e;               // Random-number engines.
-	static uniform_int_distribution<unsigned long long>  
-		                                u(0, 10000000);  // Random-number distribution.
-	vector<long long>                   v1;              // Data structure.
-	unsigned long long                  temp_key;        // Temporary data variable
-	int                                 k;               // Temporary subscript variable.
-
-	for (size_t i = 0; i != 1000; ++i) {                 // Write to the need to sort the data.
-		v1.push_back(u(e));
-	}
-
+void Insertion_Sort(vector<long long> &v1) {
+	int k;
+	long long temp_key;
 	for (size_t j = 1; j != v1.size(); ++j) {            // Algorithm.
 		temp_key = v1[j];
 		k = j - 1;
@@ -25,14 +16,30 @@ void Insertion_Sort() {
 		}
 		v1[k + 1] = temp_key;
 	}
+}
 
-	for (auto &temp : v1) {                              // Output.
-		cout << temp << " ";
+vector<long long> Random() {
+	static default_random_engine        e;               // Random-number engines.
+	static uniform_int_distribution<unsigned long long>
+		u(0, 10000000);                                  // Random-number distribution.
+	vector<long long>                   v1;              // Data structure.
+	unsigned long long                  temp_key;        // Temporary data variable
+	int                                 k;               // Temporary subscript variable.
+
+	for (size_t i = 0; i != 10000; ++i) {                // Write to the need to sort the data.
+		v1.push_back(u(e));
 	}
+
+	return v1;
 }
 
 int main(){
-	Insertion_Sort();
+    vector<long long> v1 = Random();
+	Insertion_Sort(v1);
+
+	for (auto &temp : v1) {
+	cout << temp << " ";
+	}
 
 	return 0;
 }
