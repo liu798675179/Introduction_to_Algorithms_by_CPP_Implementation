a6e52a2413fa4434aa4e787cd6dd866ccfa60c4b
Michael Liu <chyliu14@lzu.edu.cn>
2016-07-27 18:12:54 +0800

    Update Heap_Sort.cpp

--- chapter6/Heap_Sort.cpp
+++ chapter6/Heap_Sort.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <random>
 #include <vector>
@@ -10,7 +9,7 @@ vector<long long> Random() {
 	static uniform_int_distribution<long long> u(-100000000, 100000000);
 	vector<long long>                          v1;
 
-	for (size_t i = 0; i != 10000; ++i) {
+	for (auto i = 0; i != 10000; ++i) {
 		v1.push_back(u(e));
 	}
 
@@ -35,14 +34,14 @@ void Max_Heapify(vector<long long> &v1, size_t i, size_t length) {
 }
 
 void Build_Max_Heap(vector<long long> &v1) {
-	for (int i = v1.size() / 2 - 1; i != -1; --i) {
+	for (auto i = v1.size() / 2 - 1; i != -1; --i) {
 		Max_Heapify(v1, i, v1.size() - 1);
 	}
 }
 
 void Heap_Sort(vector<long long> &v1) {
 	Build_Max_Heap(v1);
-	for (int i = v1.size() - 1; i != 0; --i) {
+	for (auto i = v1.size() - 1; i != 0; --i) {
 		swap(v1[0], v1[i]);
 		Max_Heapify(v1, 0, i);
 	}
