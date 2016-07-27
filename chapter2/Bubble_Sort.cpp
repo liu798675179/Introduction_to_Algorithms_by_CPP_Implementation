cfe3899ecfac7a90d14a3c915fced07541ff274c
Michael Liu <chyliu14@lzu.edu.cn>
2016-07-27 18:24:09 +0800

    Update Bubble_Sort.cpp

--- chapter2/Bubble_Sort.cpp
+++ chapter2/Bubble_Sort.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 void Bubble_Sort(vector<long long> &v1) {
-	for (size_t i = 1; i != v1.size(); ++i) {
-		for (size_t j = v1.size() - 1; j != i - 1; --j) {
+	for (auto i = 1; i != v1.size(); ++i) {
+		for (auto j = v1.size() - 1; j != i - 1; --j) {
 			if (v1[j] < v1[j - 1]) {
 				swap(v1[j], v1[j - 1]);
 			}
@@ -19,7 +19,7 @@ vector<long long> Random() {
 	static uniform_int_distribution<long long> u(-100000000, 100000000);
 	vector<long long>                          v1;
 
-	for (size_t i = 0; i != 10000; ++i) {
+	for (auto i = 0; i != 10000; ++i) {
 		v1.push_back(u(e));
 	}
 
