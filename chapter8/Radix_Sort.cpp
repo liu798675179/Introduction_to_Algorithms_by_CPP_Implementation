7c97b848c3840dd969edd7ee28f3a7c8c7771c46
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-03 00:39:19 +0800

    Update Radix_Sort.cpp

--- chapter8/Radix_Sort.cpp
+++ chapter8/Radix_Sort.cpp
@@ -1,1 +1,49 @@
+//Tan Chuanqi's thought   chuanqi.tan@gmail.com
 
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+using namespace std;
+
+vector<long long> Random() {
+	static default_random_engine               e;
+	static uniform_int_distribution<long long> u(0, 99999);
+	vector<long long>                          v1;
+
+	for (auto i = 0; i != 10000; ++i) {
+		v1.push_back(u(e));
+	}
+
+	return v1;
+}
+
+auto max_dim = 5;
+
+void Radix_Sort(vector<long long> &v1, long long dim) { 
+	stable_sort(v1.begin(), v1.end(), [&dim](long long temp_1, long long temp_2) {
+		auto Dim_Num = [](long long &temp_num, long long &d) -> long long {
+			for (auto i = 1; i <= max_dim - d; ++i) {
+				temp_num /= 10;
+			}
+			return temp_num % 10;
+		};
+		return Dim_Num(temp_1, dim) < Dim_Num(temp_2, dim);
+	});
+
+	if (dim != 1) {
+		Radix_Sort(v1, dim - 1);
+	}
+}
+
+int main() {
+	vector<long long> v1 = Random();
+	Radix_Sort(v1, max_dim);
+
+	for (auto &i : v1) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
