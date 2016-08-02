2241e2a13e9a13c033c5b2d2e8c01da88e7f694d
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-02 17:11:01 +0800

    Update Counting_Sort.cpp

--- chapter8/Counting_Sort.cpp
+++ chapter8/Counting_Sort.cpp
@@ -1,1 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <random>
 
+using namespace std;
+
+vector<long long> Random() {
+	static default_random_engine               e;
+	static uniform_int_distribution<long long> u(0, 99999);
+	vector<long long>                          v1;
+
+	for (auto i = 0; i != 100000; ++i) {
+		v1.push_back(u(e));
+	}
+
+	return v1;
+}
+
+void Counting_sort(vector<long long> &v1) {
+	vector<long long> temp_v1 = v1;
+	vector<long long> temp_count(v1.size(), 0);
+
+	for (auto &j : v1) {
+		++temp_count[j];
+	}
+
+	for (auto i = 1; i != temp_count.size(); ++i) {
+		temp_count[i] += temp_count[i - 1];
+	}
+
+	for (auto j = v1.size() - 1; j != -1; --j) {
+		temp_v1[temp_count[v1[j]] - 1] = v1[j];
+		--temp_count[v1[j]];
+	}
+
+	v1 = temp_v1;
+}
+
+int main() {
+	vector<long long> v1 = Random();
+	Counting_sort(v1);
+
+	for (auto &i : v1) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
