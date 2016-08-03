ebad7dae896ef430533670a02bdd8be0b940856a
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-03 18:28:13 +0800

    Update Insertion_Sort.cpp

--- chapter2/Insertion_Sort.cpp
+++ chapter2/Insertion_Sort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void Insertion_Sort(vector<long long> &v1) {
 	long long k;
 	long long temp_key;
-	for (auto j = 1; j != v1.size(); ++j) {            // Algorithm.
+	for (auto j = 1; j != v1.size(); ++j) {
 		temp_key = v1[j];
 		k = j - 1;
 		while (k >= 0 && v1[k] > temp_key) {
