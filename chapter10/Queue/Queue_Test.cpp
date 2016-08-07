a5728ee37de0a7b2c69d5c98bbeb2b8c2aa5f59b
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-07 16:36:04 +0800

    Update Queue_Test.cpp

--- chapter10/Queue/Queue_Test.cpp
+++ chapter10/Queue/Queue_Test.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include "Queue.h"
+
+using namespace std;
+
+int main() {
+	Queue<int> a;
+	a.push(5);
+
+	a.push(6);
+	cout << a.back() << endl;
+	a.pop();
+	cout << a.front() << endl;
+	a.push(3);
+	a.push(5);
+	cout << a.size() << endl;
+
+	while (!a.empty()) {
+		cout << a.back() << " ";
+		a.pop();
+	}
+
+	return 0;
+}
 
