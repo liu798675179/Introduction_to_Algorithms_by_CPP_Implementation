d5c0cf3f1ddf83aeefb301579db61c74624683b4
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-12 12:09:06 +0800

    Update Hash_Table_Test.cpp

--- chapter11/Hash_Table_Test.cpp
+++ chapter11/Hash_Table_Test.cpp
@@ -9,11 +9,16 @@ int main() {
 	b.insert(78);
 	b.insert(96);
 	b.insert(96);
-	b.erase(55);
+	b.insert(96);
+	b.erase(96);
+	b.insert(79);
+	b.erase_one(55);
 	a.insert(56, 7);
 	a.insert(78, 6);
 	a.insert(96, 9);
-	a.erase(75);
+	a.insert(56, 9);
+	a.erase(56);
+	a.erase_one(99);
 
 	return 0;
 }
