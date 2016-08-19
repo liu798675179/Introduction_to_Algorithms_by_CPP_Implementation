0cc66eaad7250dc4a23189a3ea1fa3ffd599e86d
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-20 00:43:44 +0800

    Create Red_Black_Tree_Test.cpp

---
+++ chapter13/Red_Black_Tree_Test.cpp
@@ -0,0 +1,20 @@
+#include "Red_Black_Tree.h"
+
+using namespace std;
+
+int main()
+{
+	RBT RBTree;
+	for (auto i = 0; i <= 100; ++i){
+		RBTree.Insert(i);  
+	}
+	for (auto i = 0; i <= 50; ++i) {
+		RBTree.Delete(i);
+	}
+
+	RBTree.Preorder_Tree_Walk();
+
+	cout << RBTree.Tree_Hight() << endl;
+
+	return 0;
+}
