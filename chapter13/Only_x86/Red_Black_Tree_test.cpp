f4fc1fc9cbbbf222a26af472e0fed7e7f346bffc
Michael Liu <chyliu14@lzu.edu.cn>
2016-08-20 13:40:01 +0800

    Update Red_Black_Tree_test.cpp

--- chapter13/Only_x86/Red_Black_Tree_test.cpp
+++ chapter13/Only_x86/Red_Black_Tree_test.cpp
@@ -1,17 +1,15 @@
-#include "Red_Black_Tree.h"
+#include "Red_Black_Tree_x86.h"
 
 using namespace std;
 
-int main()
-{
+int main() {
 	RBT<int> RBTree;
-	for (auto i = 1; i <= 100; ++i) {
+	for (auto i = 0; i <= 100; ++i) {
 		RBTree.Insert(i);
 	}
-	for (auto i = 1; i <= 50; ++i) {
+	for (auto i = 0; i <= 50; ++i) {
 		RBTree.Delete(i);
 	}
-
 	RBTree.Preorder_Tree_Walk();
 
 	cout << RBTree.Tree_Hight() << endl;
