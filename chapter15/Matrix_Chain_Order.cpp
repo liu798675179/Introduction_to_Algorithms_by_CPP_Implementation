024540e955b7acd8d4ada04e7014658c6526f5f2
Michael Liu <chyliu14@lzu.edu.cn>
2016-11-06 15:08:04 +0800

    Update Matrix_Chain_Order.cpp

--- chapter15/Matrix_Chain_Order.cpp
+++ chapter15/Matrix_Chain_Order.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 vector<int> temp_Vec{ 30,35,15,5,10,20,25 }; //A1(30X35), A2(35X15.).....A6(20X25)
 
+//Bottom-up.
 pair<vector<vector<int>>, vector<vector<int>>> Matrix_Chain_Order(const vector<int> &temp_Vec) {
 	auto temp_n = temp_Vec.size() - 1;
 	vector<vector<int>> temp_VecM, temp_VecS;
@@ -43,6 +44,48 @@ pair<vector<vector<int>>, vector<vector<int>>> Matrix_Chain_Order(const vector<i
 	return make_pair(temp_VecM, temp_VecS);
 }
 
+//Memoized of part1.
+int Lookup_Chain(vector<vector<int>> &temp_VecM, const vector<int> &temp_Vec, const int &i, const int &j) {
+	if(temp_VecM[i][j] < INT_MAX) {
+		return temp_VecM[i][j];
+	}
+	if(i == j) {
+		temp_VecM[i][j] = 0;
+	}
+	else {
+		for(auto k = i; k <= j - 1; ++k) {
+			auto temp_q = Lookup_Chain(temp_VecM, temp_Vec, i, k) + Lookup_Chain(temp_VecM, temp_Vec, k + 1, j) + temp_Vec[i - 1] * temp_Vec[k] * temp_Vec[j];
+
+			if(temp_q < temp_VecM[i][j]) {
+				temp_VecM[i][j] = temp_q;
+			}
+		}
+	}
+
+	return temp_VecM[i][j];
+}
+
+//Memoized of part2.
+int Memoized_Matrix_Chain(const vector<int> &temp_Vec) {
+	auto temp_n = temp_Vec.size() - 1;
+	vector<vector<int>> temp_VecM;
+
+	temp_VecM.resize(temp_n + 1);
+
+	for (auto &i : temp_VecM) {
+		i.resize(temp_n + 1);
+	}
+
+	for(auto &i : temp_VecM) {
+		for(auto &j : i) {
+			j = INT_MAX;
+		}
+	}
+
+	return Lookup_Chain(temp_VecM, temp_Vec, 1, temp_n);
+}
+
+//Print optimal bracket scheme.
 void Print_Optimal_Parens(const vector<vector<int>> &temp_VecS, const int &i, const int &j) {
 	if(i == j) {
 		cout << "A" << i << " ";
@@ -59,17 +102,19 @@ int main() {
 	pair<vector<vector<int>>, vector<vector<int>>> temp_Pair = Matrix_Chain_Order(temp_Vec);
 	vector<vector<int>> temp_VecM = temp_Pair.first, temp_VecS = temp_Pair.second;
 
-	cout << temp_VecM[2][5] << endl;
+	cout << temp_VecM[2][5] << endl;  //A2 to A5 by bottom-up.
 
-	Print_Optimal_Parens(temp_VecS, 2, 5);
+	Print_Optimal_Parens(temp_VecS, 2, 5);  //A2 to A5 by bottom-up.
 
 	cout << endl << endl;
-	for(auto &i : temp_VecM) {
+	for(auto &i : temp_VecM) {  //All of the value.
 		for(auto &j : i) {
 			cout << j << " ";
 		}
 		cout << endl;
 	}
+
+	cout << endl << Memoized_Matrix_Chain(temp_Vec);  //A1 to A6 by memoized.
 
 	return 0;
 }
