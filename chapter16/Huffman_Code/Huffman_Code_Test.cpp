edc6baa51d1a2d1aa97117ba3a0b6a73c883520e
Michael Liu <chyliu14@lzu.edu.cn>
2016-11-18 20:52:20 +0800

    Create Huffman_Code_Test.cpp

---
+++ chapter16/Huffman_Code/Huffman_Code_Test.cpp
@@ -0,0 +1,11 @@
+#include "Huffman_Code.h"
+
+using namespace std;
+
+int main() {
+	H_BST A;
+	A.Huffman_Code("aaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssddddddddddddddddddeeeeeeeeeeeeefgssssssssssssssqqqqqq???>>><<<!@$%&(%^$#@#@");
+	//A.Huffman_Code_File("5_Tree_Root_2_Leaf.txt");
+
+	return 0;
+}
