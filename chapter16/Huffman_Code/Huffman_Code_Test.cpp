7773bc7f520fc7041359f2b17ffdc720e572a44e
Michael Liu <chyliu14@lzu.edu.cn>
2016-11-19 00:57:34 +0800

    Update Huffman_Code_Test.cpp

--- chapter16/Huffman_Code/Huffman_Code_Test.cpp
+++ chapter16/Huffman_Code/Huffman_Code_Test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	H_BST A;
-	A.Huffman_Code("aaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssddddddddddddddddddeeeeeeeeeeeeefgssssssssssssssqqqqqq???>>><<<!@$%&(%^$#@#@");
+	A.Huffman_Code("aaaaaaaaaaaaaaaaaaaaaaaaassssssssssssssddddddddddddddddddeeeeeeeeeeeeefgssssssssssssssqqqqqq -???>>> <<<!@$%&(%^$#@#@");
 	//A.Huffman_Code_File("5_Tree_Root_2_Leaf.txt");
 
 	return 0;
