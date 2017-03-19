1e06ac6f0aa340e99c175044bf049286feb7f5e2
Michael Liu <chyliu14@lzu.edu.cn>
2017-03-19 17:07:21 +0800

    Update Insertion_Sort.cpp

--- chapter2/Insertion_Sort.cpp
+++ chapter2/Insertion_Sort.cpp
@@ -1,6 +1,7 @@
 #include <random>
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
@@ -19,9 +20,9 @@ void Insertion_Sort(vector<long long> &v1) {
 }
 
 vector<long long> Random() {
-	static default_random_engine               e;
-	static uniform_int_distribution<long long> u(-100000000, 100000000);
-	vector<long long>                          v1;
+	default_random_engine               e(time(nullptr));
+	uniform_int_distribution<long long> u(-100000000, 100000000);
+	vector<long long>                   v1;
 
 	for (auto i = 0; i != 10000; ++i) {
 		v1.push_back(u(e));
