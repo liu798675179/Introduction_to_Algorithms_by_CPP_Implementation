330fadd9f395a0a6c9898a258350f2089ab6d426
Michael Liu <chyliu14@lzu.edu.cn>
2017-03-19 17:14:52 +0800

    Update Priority_Queue_Test.cpp

--- chapter6/Priority_Queue/Priority_Queue_Test.cpp
+++ chapter6/Priority_Queue/Priority_Queue_Test.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <ctime>
 #include "Priority_Queue.h"
 
 using namespace std;
 
 vector<long long> Random() {
-	static default_random_engine               e;
-	static uniform_int_distribution<long long> u(-100000000, 100000000);
-	vector<long long>                          v1;
+	default_random_engine               e(time(nullptr));
+	uniform_int_distribution<long long> u(-100000000, 100000000);
+	vector<long long>                   v1;
 
 	for (auto i = 0; i != 10000; ++i) {
 		v1.push_back(u(e));
