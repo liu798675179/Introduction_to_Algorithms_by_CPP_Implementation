a8d28d7ee9187672be330a686760cb2e3bdf2729
Michael Liu <chyliu14@lzu.edu.cn>
2017-03-19 17:11:15 +0800

    Update Merge_Sort.cpp

--- chapter2/Merge_Sort.cpp
+++ chapter2/Merge_Sort.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
 vector<long long> Random() {
-	static default_random_engine               e;
-	static uniform_int_distribution<long long> u(-100000000, 100000000);
-	vector<long long>                          v1;
+	default_random_engine               e(time(nullptr));
+	uniform_int_distribution<long long> u(-100000000, 100000000);
+	vector<long long>                   v1;
 
 	for (auto i = 0; i != 10000; ++i) {
 		v1.push_back(u(e));
